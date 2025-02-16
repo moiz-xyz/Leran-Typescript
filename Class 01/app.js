0928574f2259f0f61064147a95c19754e08cac24
Your Name <you@example.com>
2025-02-17 00:53:33 +0500

    class 01 added

---
+++ Class 01/app.js
@@ -0,0 +1,3 @@
+// ts compiler
+var myName = 'Abdul Moiz';
+console.log(myName);
---
+++ Class 01/app.ts
@@ -0,0 +1,18 @@
+// ts compiler
+
+// let myName : string = 'Abdul Moiz';
+// console.log(myName);
+
+// declaring variabkes 
+
+let greeting : string = "hello typescript";
+let months :number = 12;
+let daysInYear :number = 365;
+let isActive :Boolean = false;
+
+// arrays in ts 
+let fruits : string[] = ["apple" , "mango" ]
+let number : Number [] = [ 1,2,3,4,5,6, ]
+console.log(fruits);
+console.log(number);
+
